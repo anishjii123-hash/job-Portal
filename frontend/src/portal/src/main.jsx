7e205775dce9414c37a1db4a8707ab152b7cbdad
Anish Kumar Sharma <anishjii123@gamil.com>
2026-02-15 12:52:00 +0530

    persist import error

--- frontend/src/portal/src/main.jsx
+++ frontend/src/portal/src/main.jsx
@@ -7,9 +7,9 @@ import { Toaster } from "sonner";
 import { Provider } from 'react-redux'
 import store from './redux/store.js';
 import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
+//import { PersistGate } from 'redux-persist/integration/react';
 
-const persistor = persistStore(store)
+//const persistor = persistStore(store)
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
