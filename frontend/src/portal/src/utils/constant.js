b1a156c0d4fbc7ae996e43e6e319d4aac2cebe30
Anish Kumar Sharma <anishjii123@gamil.com>
2026-03-11 12:19:36 +0530

    correct api

--- frontend/src/portal/src/utils/constant.js
+++ frontend/src/portal/src/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT = "https://job-portal-lac-five.vercel.app//api/v1/user";
-export const JOBS_API_END_POINT = "https://job-portal-lac-five.vercel.app//api/v1/job";
-export const APPLY_API_END_POINT = "https://job-portal-lac-five.vercel.app//api/v1/application";
-export const COMPANY_API_END_POINT = "https://job-portal-lac-five.vercel.app//api/v1/company";
+export const USER_API_END_POINT = "https://job-portal-lac-five.vercel.app/api/v1/user";
+export const JOBS_API_END_POINT = "https://job-portal-lac-five.vercel.app/api/v1/job";
+export const APPLY_API_END_POINT = "https://job-portal-lac-five.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-portal-lac-five.vercel.app/api/v1/company";
 
 //https://job-portal-wheat-three.vercel.app/api/v1/company
 //https://job-portal-lac-five.vercel.app/
