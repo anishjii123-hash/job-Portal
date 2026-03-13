1574c3510eb625ad5155561933eecf043a2a8a73
Anish Kumar Sharma <anishjii123@gamil.com>
2026-03-13 11:21:53 +0530

    build

--- frontend/src/portal/vite.config.js
+++ frontend/src/portal/vite.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from "vite";
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react";
-//import path from "path";
-//import { createTailwindMerge } from "tailwind-merge";
+// import { defineConfig } from "vite";
+// import path from "path"
+// import tailwindcss from "@tailwindcss/vite"
+// import react from "@vitejs/plugin-react";
+// //import path from "path";
+// //import { createTailwindMerge } from "tailwind-merge";
 
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
